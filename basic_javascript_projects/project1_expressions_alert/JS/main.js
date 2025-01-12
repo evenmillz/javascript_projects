df84abefc8ad565323ef32690274a388e034da58
Even Richardson <121592609+evenmillz@users.noreply.github.com>
2025-01-11 18:55:52 -0800

    Step 47 assignment
    + Updated the first document.write() string I created in 'main.js'
    
    + Created a variable and called it using document.write()

--- basic_javascript_projects/project1_expressions_alert/JS/main.js
+++ basic_javascript_projects/project1_expressions_alert/JS/main.js
@@ -1,2 +1,4 @@
-window.alert("Window.Alert Method!")
-document.write("document.write method!")
+window.alert("This was written with the window.alert method!")
+document.write("document.write wrote this sentence, and the following text is a result of a variable named 'P' + document.write : ")
+var P = "'P' is for 'Programming'!"
+document.write(P)
