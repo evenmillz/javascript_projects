e07214e0dc5af59c247469ecad0606593e6b8b51
Even Richardson <121592609+evenmillz@users.noreply.github.com>
2025-01-11 20:14:16 -0800

    Escape Character assignment
    + Created a string that contains an apostrophe and quotation marks, using the '\' to escape characters contain within the string.
    
    + Updated preexisting code to create a more look more appealing.

--- basic_javascript_projects/project1_expressions_alert/JS/main.js
+++ basic_javascript_projects/project1_expressions_alert/JS/main.js
@@ -1,9 +1,11 @@
 window.alert("This was written solely with the window.alert method!")
 
-document.write("document.write wrote this sentence, and the following text is a result of a variable named 'P' + document.write : ")
+document.write("document.write wrote this sentence, and the following text is a result of a variable named 'P' + document.write: <br>")
 
-var P = "'P' is for 'Programming'!"
+var P = "'P' is for 'Programming'! "
 document.write(P)
 
 var S = "This text was created by assigning a string value to a variable, and then using the window.alert() method to display it!"
 window.alert(S)
+
+document.write("<br><br>What did the string say when another string asked,<br> \"Hey string! Do you happen to know the time?\"<br>\"No, I'm a frayed knot.")
