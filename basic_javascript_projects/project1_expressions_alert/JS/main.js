75dfebd3ecec6a720b050891cddf683c09fb6e13
Even Richardson <121592609+evenmillz@users.noreply.github.com>
2025-01-11 19:11:18 -0800

    Updated 'main.js'
    + Updated the first window.alert() method string
    
    + Added a string by assigning it to a variable, then called it using the window.alert() method to display it

--- basic_javascript_projects/project1_expressions_alert/JS/main.js
+++ basic_javascript_projects/project1_expressions_alert/JS/main.js
@@ -1,4 +1,9 @@
-window.alert("This was written with the window.alert method!")
+window.alert("This was written solely with the window.alert method!")
+
 document.write("document.write wrote this sentence, and the following text is a result of a variable named 'P' + document.write : ")
+
 var P = "'P' is for 'Programming'!"
 document.write(P)
+
+var S = "This string was created by assigning a string value to a variable, and then using the windo.alert() method to display it!"
+window.alert(S)
