6fd4fcf2f26c8d41ab613f7b57ab4db7037c34b8
Even Richardson <121592609+evenmillz@users.noreply.github.com>
2025-01-13 23:05:59 -0800

    Operater Assignment
    + In 'index.html', utilized the id attribute inside of an element
    
    + In 'main.js', utilized the += operator to concatenate a string, and then used the document.getElementByID method to display the concatenated string
    
    + Commented the code in both 'index.html' and 'main.js'

--- basic_javascript_projects/project2_functions/JS/main.js
+++ basic_javascript_projects/project2_functions/JS/main.js
@@ -6,3 +6,13 @@ function JS_Function_By_Even() {
     document.getElementById("button").innerHTML = jsButton;
     document.getElementById("message").innerHTML = jsMessage;
 }
+
+// Created variable and utilized the += operator to concatenate a string
+function bigConc() {
+    var words = "You have unleashed";
+    words += " Big Conc's concatenated string!";
+
+// Utilized the document.getElementByID method to display the concatenated 
+// string
+    document.getElementById("BigCwords").innerHTML = words;
+}
