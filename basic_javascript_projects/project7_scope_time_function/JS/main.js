5a2e7322e3b1ab01f188d875b26c89c4251e15c4
Even Richardson <121592609+evenmillz@users.noreply.github.com>
2025-01-22 23:56:27 -0800

    Method Assignment
    + In 'index.html', I:
    
         - Created an id attribute for 'greetUser' and an onmouseover
           attribute for 'time_Check' within a <p> element to display a
           string in the browser if it is later than 10pm
    
    + In 'main.js', I:
    
         - Created a function with an if statement that utilizes
            the new Date().getHours() method
    
         - Used the document.getElementById() method to reference the
            id attribute 'greetUser' to display a message if it is greater
            than 10pm

--- basic_javascript_projects/project7_scope_time_function/JS/main.js
+++ basic_javascript_projects/project7_scope_time_function/JS/main.js
@@ -1,3 +1,5 @@
+// SCOPE
+
 // Created a global variable
 
 let A = 1;
@@ -30,5 +32,21 @@ function Multi_ply_4() {
 
 Multi_ply_4();
 
+
+// TIME
+
+// Created a function with an if statement that utilizes
+// the new Date().getHours() method
+
+// Used the document.getElementById() method to reference 
+// the id attribute 'greetUser' to display a message if it 
+// is greater than 10pm
+
+function time_Check() {
+    if (new Date().getHours() > 22) {
+        document.getElementById("greetUser").innerHTML = 
+        "Most people are sleeping by now!"
+    }
+}
 
 
