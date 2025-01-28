506d2d6ef3bb257b408fa23805ef3ecd4569173b
Even Richardson <121592609+evenmillz@users.noreply.github.com>
2025-01-28 02:03:39 -0800

    Bug fixes
    + Updated main.js to ensure the dot associated with the slide was
       active and that the other dots remained inactive if their respective
       slide was not showing.
    
    + Updated the main.css to make sure the .number-text and the .text
       (caption)  so it didn't change to an I-cursor when hovered over

--- basic_javascript_projects/project9_countdown_slideshow/Slideshow_Assignment/JS/main.js
+++ basic_javascript_projects/project9_countdown_slideshow/Slideshow_Assignment/JS/main.js
@@ -16,14 +16,16 @@ function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
+
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
+
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className.replace(" active", "");
+        dots[i].classList.remove("active");
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].classList.add("active");
 }
