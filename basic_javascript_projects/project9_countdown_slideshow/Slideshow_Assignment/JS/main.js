c8bd975a5ddb1b4db839722fcb54cb665850f5b3
Even Richardson <121592609+evenmillz@users.noreply.github.com>
2025-01-28 02:13:41 -0800

    Update main.js
    + Added comments to the end of the code

--- basic_javascript_projects/project9_countdown_slideshow/Slideshow_Assignment/JS/main.js
+++ basic_javascript_projects/project9_countdown_slideshow/Slideshow_Assignment/JS/main.js
@@ -12,20 +12,27 @@ function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
+// Coontrols the visibility of slides and highlits the active dot
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
-
+    
+// Loops back to the first slide if the index exceeds the total slides
     if (n > slides.length) {slideIndex = 1}
+
+// Loops to the last slide if the index decrements below 1
     if (n < 1) {slideIndex = slides.length}
 
+// Hides all slides so only the active one is displayed
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
+// Removes the 'active' class from all dots
     for (i = 0; i < dots.length; i++) {
         dots[i].classList.remove("active");
     }
+// Displays the active slide and highlights the dot relative to it
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].classList.add("active");
 }
